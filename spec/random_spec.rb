b71979706f29c85b668ccedf01c95b58b5c39946
jamie rose <jrose111284@gmail.com>
2016-02-17 09:57:56 +0000

    added random spec

---
+++ spec/random_spec.rb
@@ -0,0 +1,11 @@
+require 'random'
+
+describe Random do
+  # subject(:checkout) { described_class.new( './hipstercoffee.json') }
+
+ # context 'when adding up coffee items' do
+ #
+ #  it 'can add price of coffee to order with tax' do
+ #    checkout.scan("Cafe Latte")
+ #    expect(checkout.total).to eq(5.15)
+  end
